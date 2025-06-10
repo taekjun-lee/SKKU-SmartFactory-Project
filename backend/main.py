332db7920536e2689cea5d091c1f874bc36d848e
taekjun-lee <leetj94@skku.edu>
2025-06-10 17:09:04 +0900

    Update

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://taekjun-lee.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
